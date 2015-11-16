99730d43affda71104df16de3aff0b50581e2148
Evan Lin <evanslin@gmail.com>
2015-11-16 13:34:06 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+
+xcodeproj 'ck101'
+
+pod 'DKImageBrowser'
---
+++ ck101/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ck101
+//
+//  Created by Evan Lin on 2015/11/16.
+//  Copyright © 2015年 evanlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
